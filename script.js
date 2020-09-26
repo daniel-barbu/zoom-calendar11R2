a47793509c0b0034015292856b6608b5243e1a8c
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-09-26 18:40:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,20 +15,21 @@ var hourTable=[["-1", "SC", "INFORMATICA", "INFORMATICA", "SPORT", "FRANCEZA"], 
                 ["GEOGRAFIE", "ROMANA", "FIZICA", "FIZICA", "FRANCEZA", "ECONOMIE"],                      //wednesday
                 ["RELIGIE", "BIOLOGIE", "INFORMATICA", "INFORMATICA", "ENGLEZA", "CHIMIE", "MATEMATICA"], //thursday
                 ["ISTORIE", "MATEMATICA", "ROMANA", "ROMANA", "ENGLEZA", "ENGLEZA"]];                     //friday
- 
-var day=new Date().getDay()-1;//0-4
-var min=new Date().getHours()*60+new Date().getMinutes(), hStart, hEnd, hNow=new Date().getHours()+":"+new Date().getMinutes(), hour; //0-6
-if (445 <= min && min < 490) {hour=0; hStart="7:30"; hEnd="8:10";}
-else if (490 <= min && min < 535) {hour=1; hStart="8:15"; hEnd="8:55";}
-else if (535 <= min && min < 580) {hour=2; hStart="9:00"; hEnd="9:40";}
-else if (580 <= min && min < 625) {hour=3; hStart="9:45"; hEnd="10:25";}
-else if (625 <= min && min < 670) {hour=4; hStart="10:30"; hEnd="11:10";}
-else if (670 <= min && min < 715) {hour=5; hStart="11:15"; hEnd="11:55";}
-else if (715 <= min && min < 760) {hour=6; hStart="12:00"; hEnd="12:40";}
 
 function parseID(x) {return x.substr(0, 3) + " " + x.substr(3, 3) + " " + x.substr(6, 4);}
 
-day=1; hour=2;
+main(); setTimeout(function(){setInterval(main, 60000);}, (60-new Date().getSeconds())*1000);
+function main() {
+var day=new Date().getDay()-1;//0-4 
+var min=new Date().getHours()*60+new Date().getMinutes(), hStart, hEnd, hNow, hour; //0-6
+if (new Date().getMinutes()<10) hNow=new Date().getHours()+":0"+new Date().getMinutes(); else hNow=new Date().getHours()+":"+new Date().getMinutes();
+if (445 <= min && min < 490) {hour=0; hStart="7:30"; hEnd="8:10"; perc=(min-450)/40;}
+else if (490 <= min && min < 535) {hour=1; hStart="8:15"; hEnd="8:55"; perc=(min-495)/40;}
+else if (535 <= min && min < 580) {hour=2; hStart="9:00"; hEnd="9:40"; perc=(min-540)/40;}
+else if (580 <= min && min < 625) {hour=3; hStart="9:45"; hEnd="10:25"; perc=(min-585)/40;}
+else if (625 <= min && min < 670) {hour=4; hStart="10:30"; hEnd="11:10"; perc=(min-630)/40;}
+else if (670 <= min && min < 715) {hour=5; hStart="11:15"; hEnd="11:55"; perc=(min-675)/40;}
+else if (715 <= min && min < 760) {hour=6; hStart="12:00"; hEnd="12:40"; perc=(min-720)/40;}
 
 if (typeof idTable[day] == "undefined" || typeof idTable[day][hour] == "undefined") {
   document.getElementById("hourInfo").innerHTML="IN AFARA PROGRAMULUI";
@@ -39,8 +40,9 @@ if (typeof idTable[day] == "undefined" || typeof idTable[day][hour] == "undefine
   document.getElementById("appLink").href="";}
 else {
   document.getElementById("hourInfo").innerHTML="Ora actuala: " + hourTable[day][hour];
-  document.getElementById("hourProgress").innerHTML="|-----------------------------------------------|<br>" + hStart + "---" + hNow + "---" + hEnd;
+  document.getElementById("hourProgress").innerHTML="|" + "-".repeat(perc*70) + "|" + "-".repeat((1-perc)*70) + "|<br>" + hStart + " "+ ".".repeat(perc*72) + " " + hNow + " "+ ".".repeat((1-perc)*72) + " " + hEnd;
   document.getElementById("idInfo").innerHTML="Meeting ID: " + parseID(idTable[day][hour]);
   document.getElementById("passInfo").innerHTML="Passcode: " + passTable[day][hour];
   document.getElementById("browserLink").href="http://zoom.us/wc/join/" + idTable[day][hour];
   document.getElementById("appLink").href="zoommtg://zoom.us/join?action=join&confno=" + idTable[day][hour];}
+}
